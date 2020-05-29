784441e61356266eae0c572e2522d14a94454422
Chris Kimani <62855363+Chriskimani@users.noreply.github.com>
2020-05-29 21:38:34 +1000

    Update app.py
    Added a few more pages to the basic app. getting there...

--- app.py
+++ app.py
@@ -6,11 +6,29 @@ from flask import Flask
 app = Flask (__name__)
 
 #the below code is a decorator (provides additional functionalist to an existing object without modifying its structure)
+# the "/" below is what is known as "root" directory or index. Changing this would result in a different url i.e. if you change it to "app" the url will be http://127.0.0.1:5000/App
 @app.route('/')
 def welcome():
     return "hello world, this is my first flask app"
 
+# you can't have the same function name
+@app.route('/page1')
+def page1():
+    return "this is my first page"
 
+@app.route('/page2')
+def page2():
+    return "This is my second page"
+
+#Main difference between POST and GET method is that GET carries request parameter appended in URL string while POST 
+#carries request parameter in message body which makes it more secure way of transferring data from client to server 
+#in http protocol. Basically, if you want a more secure connection, use "POST"
+@app.route('/method', methods =['GET','POST'])
+def method():
+    if method == 'POST':
+        return "you've used a post request."
+    else:
+        return  "You are using the Get Method"
 
 
 # the code below will run the flask application
